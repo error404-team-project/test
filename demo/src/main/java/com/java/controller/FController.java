971c9248099f87f8f51d6b2805f17a54d14f3139
sepangE <166789103+sepangE@users.noreply.github.com>
2024-08-25 01:05:07 +0900

    24/08/24 사이드바 조금 더 수정

--- demo/src/main/java/com/java/controller/FController.java
+++ demo/src/main/java/com/java/controller/FController.java
@@ -1,16 +1,31 @@
 package com.java.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FController {
 
 	
-@RequestMapping ("/index")
-public String index () {
-	return "////////////////////////index";
+	@RequestMapping ("/index")
+	public String index () {
+		return "index";
+	}
 	
-}
+	@RequestMapping ("/admin_index")
+	public String admin_index () {
+		return "admin_index";
+	}
+	
+	@RequestMapping ("/chartjs")
+	public String chartjs () {
+		return "chartjs";
+	}
+
+	@RequestMapping("/ex_f_board")
+	public String ex_f_board() {
+		return "ex_f_board";
+	}
 
 }
--- demo/src/main/webapp/WEB-INF/views/index.jsp
+++ demo/src/main/webapp/WEB-INF/views/index.jsp
@@ -10,6 +10,7 @@
 	<h1>부에에에엑</h1>
 	<h1>부엑</h1>
 	<h1>가보자고</h1>
-
+	<a href="/admin_index">관리자 메인페이지</a> <br>
+	<a href="/chartjs">차트</a>
 </body>
 </html>

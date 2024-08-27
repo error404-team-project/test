7b73304c389e0923708750153396898882df1024
KOREAVC <KOREAVC@DESKTOP-6Q74T2G>
2024-08-27 19:50:56 +0900

    commmon.js 코드 일부분 수정

--- demo/src/main/resources/static/js/common.js
+++ demo/src/main/resources/static/js/common.js
@@ -43,33 +43,31 @@ $(function(){
 		}
 	});
 
-	
+
+  
 	$.fn.extend({
-		searchStyle : function(options) {
-			this.each(function() {
+    searchStyle: function(options) {
+        this.each(function() {
+            var currentSelected = $(this).find(':selected');
+            $(this).after();
 
-				var currentSelected = $(this).find(':selected');
-				$(this).after('<span class="searchStyleSelectBox"><span class="searchStyleSelectBoxInner">'+currentSelected.text()+'</span></span>').css({position:'absolute', opacity:0,fontSize:$(this).next().css('font-size')});
-				
-				var selectBoxSpan = $(this).next();
-				var selectBoxWidth = parseInt($(this).width()) - parseInt(selectBoxSpan.css('padding-left'));   
-				
-				var selectBoxSpanInner = selectBoxSpan.find(':first-child');
-				selectBoxSpan.css({display:'inline-block'});
-				selectBoxSpanInner.css({width:selectBoxWidth, display:'inline'});
+            var selectBoxSpan = $(this).next();
+            var selectBoxWidth = parseInt($(this).width());
 
-				var selectBoxHeight = parseInt(selectBoxSpan.height()) + parseInt(selectBoxSpan.css('padding-top')) + parseInt(selectBoxSpan.css('padding-bottom'));
-				
-				var selectWidth = $(this).width();
-				$(this).css("width",selectWidth+40+"px");
+            var selectBoxSpanInner = selectBoxSpan.find(':first-child');
+            
+            selectBoxSpanInner.css({ width: selectBoxWidth, display: 'inline' });
 
-				$(this).height(selectBoxHeight).change(function(){
-					selectBoxSpanInner.text($(this).find(':selected').text()).parent().addClass('changed');
-					});
-				});
-			
-		}
-	});
+            var selectBoxHeight = parseInt(selectBoxSpan.height());
+
+            $(this).change(function() {
+                selectBoxSpanInner.text($(this).find(':selected').text()).parent().addClass('changed');
+            });
+        });
+    }
+});
+
+
 
 	$('select').searchStyle();
 

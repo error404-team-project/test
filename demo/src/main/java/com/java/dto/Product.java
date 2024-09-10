5642d27feab43ded15d20c461930189d09d33533
sepangE <hphk178@naver.com>
2024-09-10 15:56:09 +0900

    어제 상품 dto만 안올림 다른거 다바꿔놓고

--- demo/src/main/java/com/java/dto/Product.java
+++ demo/src/main/java/com/java/dto/Product.java
@@ -8,8 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-	private int p_num, product_category, medical_category, daily_category, price, stock, bookmark, basket, p_count;
-	private String name, company, image, howuse, description, medical_precautions,side_effects, storage, medical_becareful, row_material;
-	private Page page;
+	private Integer p_num, product_category, medical_category, health_category, price, stock, p_count;
+	private String name, company, image, howuse, description, precautions,side_effects, storage, becareful, row_material;
 	
 }

465c2dd30d7f5fd3e93e8f92eba8ed6b11e1b913
sepangE <hphk178@naver.com>
2024-09-26 18:15:06 +0900

    sdf

--- demo/src/main/java/com/java/dto/Product.java
+++ demo/src/main/java/com/java/dto/Product.java
@@ -8,7 +8,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-	private Integer p_num, product_category, medical_category, health_category, price, stock, p_count;
-	private String name, company, image, howuse, description, precautions,side_effects, storage, becareful, row_material;
+	private Integer p_num;
+	private Integer product_category;
+	private Integer medical_category;
+	private Integer health_category;
+	private Integer price;
+	private Integer stock;
+	private Integer p_count;
+	private Integer user_seq;
+	private double weight;
+	private String name;
+	private String company;
+	private String image;
+	private String howuse;
+	private String description;
+	private String precautions;
+	private String side_effects;
+	private String storage;
+	private String becareful;
+	private String row_material;
 	
 }
